cb0a76f61a679a0b1fbf4a61f0b87ce7bc809d1f
Thomas Ledoux <tledoux@reference.be>
2020-01-27 17:10:17 +0100

    Update personal section

--- pages/navigation.js
+++ pages/navigation.js
@@ -1,29 +1,32 @@
 const Navigation = () => {
     return (
         <nav>
-            <a href="#" className="logo">
-                <div className="letter">t</div>
-                <div className="letter">h</div>
-                <div className="letter">m</div>
-                <div className="letter">s</div>
-                <div className="letter">l</div>
-                <div className="letter">d</div>
-                <div className="letter">x</div>
-            </a>
-            <ul>
-                <li>
-                    <a href="#personal">Personal</a>
-                </li>
-                <li>
-                    <a href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#freetime">Free Time</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
+            <div className="navigation-wrapper">
+                <a href="#" className="logo">
+                    <div className="letter">t</div>
+                    <div className="letter">h</div>
+                    <div className="letter">m</div>
+                    <div className="letter">s</div>
+                    <div className="letter">l</div>
+                    <div className="letter">d</div>
+                    <div className="letter">x</div>
+                </a>
+                <ul className="navigation-desktop">
+                    <li>
+                        <a href="#personal">Personal</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#freetime">Free Time</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+                <span className="navigation-mobile"></span>
+            </div>
         </nav>
     )
 };
