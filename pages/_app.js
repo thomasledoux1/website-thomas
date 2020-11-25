406ad522d9418c1e156d7dfbce81b0f01d334a73
thomasledoux1 <thomasledoux1@gmail.com>
2020-11-25 17:28:27 +0100

    Attempting to fix ios vh bug

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ import Layout from '../components/Layout';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     useEffect(() => {
+        document.querySelector(':root').style
+            .setProperty('--vh', window.innerHeight / 100 + 'px');
         window.addEventListener('resize', () => {
             document.querySelector(':root').style
                 .setProperty('--vh', window.innerHeight / 100 + 'px');
