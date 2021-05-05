c72a0750f07b7154a3c7e3f8b990ecc9fa4e6753
thomasledoux1 <thomasledoux1@gmail.com>
2021-05-05 20:42:15 +0200

    Fix text color mobile nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -102,7 +102,7 @@ const Navigation = () => {
         <ul className="hidden md:flex">{renderNavigationItems()}</ul>
         <ul
           ref={navigationMobileRef}
-          className={`md:hidden absolute flex flex-col w-full top-16 left-0 py-3 items-center bg-darkPurple dark:bg-orange transform translate-x-full ${
+          className={`md:hidden absolute flex flex-col w-full top-16 left-0 py-3 items-center text-white bg-darkPurple dark:bg-orange transform translate-x-full ${
             isMounted.current ? 'transition-transform' : ''
           }`}
         >
