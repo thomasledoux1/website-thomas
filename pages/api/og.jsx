a8c5e97ca625a443417de4361dd467806a234622
Thomas Ledoux <tledoux@reference.be>
2022-10-18 19:09:31 +0200

    Don't use tsx

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -1,30 +1,30 @@
-import { ImageResponse } from '@vercel/og';
+import {ImageResponse} from '@vercel/og'
 
 export const config = {
   runtime: 'experimental-edge',
-};
+}
 
-export default function handler(req: any) {
+export default function handler(req) {
   try {
-    const { searchParams } = new URL(req.url);
+    const {searchParams} = new URL(req.url)
 
     // ?title=<title>
-    const hasTitle = searchParams.has('title');
+    const hasTitle = searchParams.has('title')
     const title = hasTitle
       ? searchParams.get('title')?.slice(0, 100)
-      : 'My default title';
+      : 'My default title'
 
     return new ImageResponse({
       type: 'div',
       props: {
         children: title,
-        style: { color: 'black' },
+        style: {color: 'black'},
       },
-    });
-  } catch (e: any) {
-    console.log(`${e.message}`);
+    })
+  } catch (e) {
+    console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
       status: 500,
-    });
+    })
   }
 }
