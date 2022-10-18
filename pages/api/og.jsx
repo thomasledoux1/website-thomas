310a5f4e565204debbae5b1b75663a973e83f0c9
Thomas Ledoux <tledoux@reference.be>
2022-10-18 19:40:32 +0200

    Adding some styling + title

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -14,13 +14,42 @@ export default function handler(req) {
       ? searchParams.get('title')?.slice(0, 100)
       : 'My default title'
 
-    return new ImageResponse({
-      type: 'div',
-      props: {
-        children: title,
-        style: {color: 'black'},
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            background: 'white',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            backgroundImage:
+              'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+            backgroundSize: '100px 100px',
+          }}
+        >
+          <div
+            style={{
+              width: '80%',
+              display: 'flex',
+              flexDirection: 'column',
+              textAlign: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <p style={{fontSize: 32}}>Thomas Ledoux&apos;s blog</p>
+            <p style={{fontSize: 64}}>{title}</p>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 600,
       },
-    })
+    )
   } catch (e) {
     console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
